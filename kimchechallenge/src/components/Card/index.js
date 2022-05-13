7986b9b6850dd703a67d2ffface05b5838f6c785
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-05-13 15:30:18 -0400

    Feature: Connect with GraphQL server

--- kimchechallenge/src/components/Card/index.js
+++ kimchechallenge/src/components/Card/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./style.css";
 
-export const Card = () => {
+export const Card = ({name, emoji, native, capital}) => {
   return (
     <div className="card-bg">
       <div className="card-header">
-        <span role="img" aria-label="🇦🇩">
-        🇦🇩
-        </span>
-        <h3>Country</h3>
+        <span>{emoji}</span>
+        <h3>{name}</h3>
       </div>
       <div className="card-content">
-        Info
+        <h4>Native</h4>
+        <span>{native}</span>
+        <h4>Capital</h4>
+        <span>{capital}</span>
       </div>
       <div className="card-button">
         <button>Show more</button>
