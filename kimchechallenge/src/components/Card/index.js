69207f75d6aaf6ee103d8ba1d8e6368e4ec7cfb6
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-05-16 14:23:32 -0400

    Feature: Modal for country information

--- kimchechallenge/src/components/Card/index.js
+++ kimchechallenge/src/components/Card/index.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { InfoModal } from "../InfoModal";
 
-export const Card = ({name, emoji, native, capital}) => {
+export const Card = ({code, name, emoji, native, capital}) => {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className="card-bg">
-      <div className="card-header">
-        <span>{emoji}</span>
-        <h3>{name}</h3>
+    <>
+      <div className="card-bg">
+        <div className="card-header">
+          <span>{emoji}</span>
+          <h3>{name}</h3>
+        </div>
+        <div className="card-content">
+          <h4>Native</h4>
+          <span>{native}</span>
+          <h4>Capital</h4>
+          <span>{capital}</span>
+        </div>
+        <div className="card-button">
+          <button onClick={() => setShowModal(true)}>Show more</button>
+        </div>
       </div>
-      <div className="card-content">
-        <h4>Native</h4>
-        <span>{native}</span>
-        <h4>Capital</h4>
-        <span>{capital}</span>
-      </div>
-      <div className="card-button">
-        <button>Show more</button>
-      </div>
-    </div>
+      {showModal ? (<InfoModal countryCode={code} setShowModal={setShowModal} />) : ("")}
+    </>  
   );
 }
